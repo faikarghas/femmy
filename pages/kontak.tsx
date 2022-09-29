3f0abeb581f55aeb8ec7a1c0188b49a231c5e96a
faikarghas <ghassanfaikar@yahoo.com>
2022-09-29 15:47:38 +0700

    feat(under-dev): v0

---
+++ pages/kontak.tsx
@@ -0,0 +1,44 @@
+import type { NextPage } from 'next';
+import Link from 'next/link';
+
+// import component
+import Layout from '../components/layouts/index';
+import FormJoin from '../components/presentational/FormJoin/FormJoin';
+
+// import hoc
+import { withAUth } from '../hoc/withAuth';
+
+const Kontak: NextPage = () => {
+  return (
+    <Layout page="tentang-kami">
+      <main>
+        <section className="bg-[#FBEEE5]/[0.5]">
+            <div className='mb-16'>
+                <img src='/images/banner-kontak-femmy.jpg' className='mb-12'/>
+                <h3 className='text-femmy-pdark font-head text-[30px] font-semibold leading-tight text-center mb-4'>Kontak kami</h3>
+                <p className='font=sans text-[16px] text-femmy-pdark text-center mb-4'>Kontak kami untuk penjelasan lebih lanjut mengenai<br/>produk, member, maupun reseller.</p>
+            </div>
+            <div className='flex flex-wrap flex-row px-8 lg:px-32 gap-4 justify-between pb-14'>
+                <div className='basis-[32%] border-[1px] border-femmy-pdark rounded-2xl h-[200px] flex flex-col text-center items-center py-10'>
+                    <img src='/images/cs.png' className='h-[55px] mb-6'/>
+                    <span className='text-femmy-pdark font-sans font-semibold'>Customer Service/<br/>WA Business</span>
+                </div>
+                <div className='basis-[32%] border-[1px] border-femmy-pdark rounded-2xl h-[200px] flex flex-col text-center items-center py-10'>
+                    <img src='/images/marketplc.png' className='h-[55px] mb-6'/>
+                    <span className='text-femmy-pdark font-sans font-semibold'>Marketplace</span>
+                </div>
+                <div className='basis-[32%] border-[1px] border-femmy-pdark rounded-2xl h-[200px] flex flex-col text-center items-center py-10'>
+                    <img src='/images/sosmed.png' className='h-[55px] mb-6'/>
+                    <span className='text-femmy-pdark font-sans font-semibold'>Sosial Media</span>
+                </div>
+                <div className='basis-[100%] border-[1px] border-femmy-pdark rounded-2xl h-[80px] mt-2 flex justify-center items-center font-semibold'>
+                    <span className='text-femmy-pdark font-sans'>Ingin Ajukan Pertanyaan?</span>
+                </div>
+            </div>
+        </section>
+      </main>
+    </Layout>
+  );
+};
+
+export default Kontak;
