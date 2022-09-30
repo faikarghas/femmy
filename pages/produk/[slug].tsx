86e55310f10f1eb610df137bcdbf638d5eccd79f
faikarghas <ghassanfaikar@yahoo.com>
2022-09-30 18:08:03 +0700

    feat(under-dev): v0

--- pages/produk/[slug].tsx
+++ pages/produk/[slug].tsx
@@ -26,33 +26,31 @@ const Tentang: NextPage = () => {
                     <Breadcrumb page='produk' detail='Femmy Look Slim'/>
                 </div>
                 <div className="basis-full xl:basis-1/4 mb-12">
-                    <h4 className="font-head text-femmy-pdark text-[35px] font-semibold">
-                    Produk Femmy
-                    </h4>
+                    <h4 className="font-head text-femmy-pdark text-[35px] font-semibold hidden md:block">Produk Femmy</h4>
                 </div>
                 <div className="basis-full xl:basis-3/4 mb-12 flex items-center w-full">
                     <SearchProduct />
                 </div>
 
-                <div className="basis-full lg:basis-[30%] pr-8">
-                    <div className="bg-femmy-white h-[300px] w-[300px] rounded-xl flex justify-center items-center">
-                        <img src="/images/femmy-look-slim.png" className="object-contain w-[200px]"/>
+                <div className="basis-full lg:basis-[30%] pr-0 md:pr-8 mb-8">
+                    <div className="bg-femmy-white h-[300px] w-full md:w-[300px] rounded-xl flex justify-center items-center">
+                        <img src="/images/femmy-look-slim.png" className="object-contain w-full md:w-[200px]"/>
                     </div>
                 </div>
                 <div className="basis-full lg:basis-[70%]">
-                    <h5 className="font-head text-femmy-pdark text-[30px] mb-3 font-semibold">Femmy Vitamin D3</h5>
-                    <span className="font-sans text-black text-[20px] font-semibold mb-4 block">RP 22.000.-</span>
-                    <div className="border-t-[1px] border-b-[1px] border-femmy-pmedium flex justify-between items-center">
-                        <ul className="list-none flex gap-6">
-                            <li onClick={() => onShow(1)} className={`${tabId == 1 ? 'border-b-2 border-femmy-pdark':'opacity-50'} py-2 inline-block  font-sans font-bold cursor-pointer w-[120px] text-center`}>Detail</li>
-                            <li onClick={() => onShow(2)} className={`${tabId == 2 ? 'border-b-2 border-femmy-pdark':'opacity-50'} py-2 inline-block text-femmy-pdark font-sans font-bold cursor-pointer w-[120px] text-center`}>Info Penting</li>
+                    <h5 className="font-head text-femmy-pdark text-[30px] mb-3 font-semibold text-center md:text-left">Femmy Vitamin D3</h5>
+                    <span className="font-sans text-femmy-white md:text-black text-[20px] font-semibold text-center md:text-left p-2 md:p-0 mb-6 md:mb-0 mt-8 md:mt-0 block bg-femmy-pdark md:bg-femmy-pdark rounded-full"><span className='block md:hidder font-sans text-white text-[12px] font-normal'>Beli Sekarang</span>RP 22.000.-</span>
+                    <div className="md:border-t-[1px] border-b-[1px] border-femmy-pmedium flex justify-between items-center">
+                        <ul className="list-none flex gap-6 w-full">
+                            <li onClick={() => onShow(1)} className={`${tabId == 1 ? 'border-b-2 border-femmy-pdark':'opacity-50'} py-2 inline-block  font-sans font-bold cursor-pointer w-full md:w-[120px] text-center`}>Detail</li>
+                            <li onClick={() => onShow(2)} className={`${tabId == 2 ? 'border-b-2 border-femmy-pdark':'opacity-50'} py-2 inline-block text-femmy-pdark font-sans font-bold cursor-pointer w-full md:w-[120px] text-center`}>Info Penting</li>
                         </ul>
-                        <span className="inline-block text-black font-sans font-semibold flex"><img src='/images/share.png' className='mr-[13px] w-[17px] object-contain'/>Bagikan</span>
+                        <span className="text-black font-sans font-semibold  hidden md:flex"><img src='/images/share.png' className='mr-[13px] w-[17px] object-contain'/>Bagikan</span>
                     </div>
                     <div>
                         <div className={`${tabId == 1 ? 'flex' : 'hidden'} py-6`}>
                             <div className="basis-3/5">Detail</div>
-                            <div className="basis-2/5 flex justify-end">
+                            <div className="basis-2/5 justify-end hidden md:flex">
                                 <div className="bg-femmy-pdark py-6 pl-8 pr-6 rounded-2xl w-[260px]">
                                     <h6 className="text-white font-sans text-[18px] text-center mb-4 leading-tight">Pembelian dari<br/>Official Store kami</h6>
                                     <Link href=""><a><img src="/images/to-tokped.png"/></a></Link>
@@ -66,21 +64,31 @@ const Tentang: NextPage = () => {
                 </div>
             </section>
             <div className="border-b-[1px] border-femmy-pdark mx-8 lg:mx-16"></div>
+
+            <div className="w-full flex md:hidden">
+                <div className="w-full bg-femmy-pdark py-6 pl-8 pr-6 ">
+                    <h6 className="text-white font-sans text-[18px] text-center mb-4 leading-tight">Pembelian dari<br/>Official Store kami</h6>
+                    <Link href=""><a><img src="/images/to-tokped.png"/></a></Link>
+                    <Link href=""><a><img src="/images/to-blibli.png"/></a></Link>
+                    <Link href=""><a><img src="/images/to-shopee.png"/></a></Link>
+                </div>
+            </div>
+
             <section className="pb-12 pt-8 px-8 lg:px-16 bg-[#FEF7F8]">
                 <h4 className="font-head text-femmy-pdark text-[35px] mb-6 font-semibold">
                     Produk Lainnya
                 </h4>
-                <div className="flex flex-row gap-6">
-                    <div className="basis-1/4"> 
+                <div className="grid grid-cols-2 lg:grid-cols-4 gap-5 lg:gap-10">
+                    <div className=""> 
                         <CardProduct data={{ title: 'Femmy Look Slim' }} />
                     </div>
-                    <div className="basis-1/4"> 
+                    <div className=""> 
                         <CardProduct data={{ title: 'Femmy Look Slim' }} />
                     </div>
-                    <div className="basis-1/4"> 
+                    <div className=""> 
                         <CardProduct data={{ title: 'Femmy Look Slim' }} />
                     </div>
-                    <div className="basis-1/4"> 
+                    <div className=""> 
                         <CardProduct data={{ title: 'Femmy Look Slim' }} />
                     </div>
                 </div>
