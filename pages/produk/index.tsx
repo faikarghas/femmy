12edc4f77579747ff91e25cd6980492c37c41bf8
faikarghas <ghassanfaikar@yahoo.com>
2022-10-05 14:45:31 +0700

    feat(under-dev): v0

--- pages/produk/index.tsx
+++ pages/produk/index.tsx
@@ -43,7 +43,7 @@ const Tentang: NextPage = () => {
           <div className="basis-full lg:basis-3/4">
             <div className="grid grid-cols-2 lg:grid-cols-3 gap-5 lg:gap-10">
               {produk.map((val, i) => (
-                <CardProduct key={i} data={{ judul: val.judul, harga: val.harga, rating: 4, image: '' }} />
+                <CardProduct key={i} data={{ judul: val.judul, harga: val.harga, rating: 4, image: val.image }} />
               ))}
             </div>
           </div>
