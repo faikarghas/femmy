2401f28f61fc25e7005d5fdc3945b4a4886b69d6
faikarghas <ghassanfaikar@yahoo.com>
2022-10-07 20:22:07 +0700

    refactor(): update

--- pages/tips-dan-trik/[slug].tsx
+++ pages/tips-dan-trik/[slug].tsx
@@ -11,7 +11,7 @@ import { withAUth } from '../../hoc/withAuth';
 const TipsDetail: NextPage = () => {
   return (
     <Layout page="tentang-kami">
-      <section className="relative bg-[#FBEEE5]/[0.5] pt-0 lg:pt-8 lg:pb-8 px-0 md:px-16">
+      <section className="relative bg-[#FCF4EE] pt-0 lg:pt-8 lg:pb-8 px-0 md:px-16">
         <h2 className="hidden md:block font-head text-femmy-pdark text-[40px] mb-10 font-semibold">
           Tips & Trik
         </h2>
