c1371ca743936b77b7a41872e3b9d03bb7cd6b1c
faikarghas <ghassanfaikar@yahoo.com>
2022-11-03 07:38:17 +0700

    feat(): google analytic

--- components/layouts/index.tsx
+++ components/layouts/index.tsx
@@ -1,6 +1,6 @@
 import Header from '../presentational/Header/Header';
 import Footer from '../presentational/Footer/Footer';
-
+import Script from 'next/script'
 export interface ILayout {
   children: React.ReactNode;
   page: string;
@@ -9,6 +9,20 @@ export interface ILayout {
 const Layout: React.FC<ILayout> = ({ children, page }) => {
   return (
     <>
+     {/* <!-- Global site tag (gtag.js) - Google Analytics --> */}
+      <Script
+        src="https://www.googletagmanager.com/gtag/js?id=UA-245288693-1"
+        strategy="afterInteractive"
+      />
+      <Script id="google-analytics" strategy="afterInteractive">
+        {`
+          window.dataLayer = window.dataLayer || [];
+          function gtag(){window.dataLayer.push(arguments);}
+          gtag('js', new Date());
+
+          gtag('config', 'UA-245288693-1');
+        `}
+      </Script>
       <Header page={page} />
       <main className={`${page !== 'home' ? 'mt-[85px]' : ''}`}>
         {children}
