a32791606438bfc87fb5417559f3f152c8119375
Faikar Ghassan <ghassanfaikar@yahoo.com>
2024-04-17 00:43:20 +0700

    feat(): update banner

--- components/presentational/Header/Header.tsx
+++ components/presentational/Header/Header.tsx
@@ -15,16 +15,16 @@ const Header: React.FC<IHeader> = ({ page, auth }) => {
 
   if (page == 'home') {
     return (
-      <header className="relative h-screen bg-[#F4AFC2]">
+      <header className="relative lg:h-screen bg-[#F4AFC2]">
         <Nav position="header" page={page} auth={auth}/>
         <picture>
           <source
             media="(min-width:750px)"
-            srcSet="/images/femmy_banner_banner1(1).jpg"
+            srcSet="/images/HOMEBANNER-DESKTOP-3840x2171.jpg"
           />
           <img
-            src="/images/femmy_banner_mobile2m.jpg"
-            className="w-full h-full object-cover"
+            src="/images/HOMEBANNER-MOBILE-622x1133.jpg"
+            className="w-full h-full object-contain lg:object-cover"
           ></img>
         </picture>
       </header>
